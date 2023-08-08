41220757ed918f309151a53c10d2453245e16e26
Ivan <navirobayo@gmail.com>
2023-08-08 02:06:25 -0500

    Basic variables study

--- Fundamentals/basic_variables.c
+++ Fundamentals/basic_variables.c
@@ -1,13 +1,23 @@
 #include<stdio.h>
+#include <stdbool.h>
 
 int main() {
-    int testOfVariable = 24;
-    printf("This is a test on how you first"
-    "declare the position of the variable here: %d"
-    "and then you can call it again here: %d"
-    "remember to declare the variable again"
-    "if used multiple times, at the end.",
-     testOfVariable, testOfVariable);
+
+// Variables used in other languages. 
+
+    int testOfInt = 24;  // %d
+    char testOfChar = 'a'; // %c
+    char testOfString[] = "What is happening bro?"; // %s
+    float testOfFloat = 3.14; // %f - 4 Bite
+    double testOfDouble = 3.14159265358979323846; // %lf - 8 Bite    
+    bool testOfBool = true; 
+
+// More specific types of variables available in C. 
+
+printf("Esto es una prueba de un numero entero, el numero es: %d", testOfInt);
+printf("Esto es una prueba de un numero entero, el numero es: %d", testOfInt);
+
+
 
 
     return 0;
--- Fundamentals/tempCodeRunnerFile.c
+++ Fundamentals/tempCodeRunnerFile.c
@@ -1,1 +1,1 @@
-
+testOfVariable
